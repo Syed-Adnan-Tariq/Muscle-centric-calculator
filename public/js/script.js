d5f4f2cefdec7a8db7ad056d9018dee8789e2283
Syed-Adnan-Tariq <syedadnantariq08@gmail.com>
2023-01-28 21:39:10 +0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -251,8 +251,6 @@ function step8(protein, fat, carb) {
       alwaysOutside: true,
       textStyle: {
         fontSize: 11,
-        color: "#000",
-        auraColor: "none",
       },
     },
     hAxis: {
